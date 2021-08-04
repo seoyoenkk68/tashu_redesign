e0f801be902691731ba15696bcfcfa0e1a5eb91e
seoyoenkk68 <seoyeonkk68@gmail.com>
2021-08-04 15:35:53 +0900

    타슈

---
+++ docs/index.js
@@ -0,0 +1,104 @@
+gsap.from(".img_1", {
+  x: 300,
+  opacity: 0,
+  duration: 1.5,
+})
+
+
+gsap.from(".img_2", {
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_2',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_3", {
+  y: -100,
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_2',
+    start: '0, 55%'
+  }
+})
+
+gsap.from(".img_4", {
+  y:200,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_4',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_5", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_5',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_6", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_6',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_7", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_7',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_8", {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_8',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_13-16", {
+  y:100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img_13-16',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_17", {
+  opacity:0,
+  duration:4,
+  scrollTrigger:{
+    trigger:'.img_17',
+    start: '0, 60%'
+  }
+})
+
+gsap.from(".img_18", {
+  x:100,
+  opacity:0,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.img_18',
+    start: '0, 40%'
+  }
+})
